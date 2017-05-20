a096dbbcf6f0d729c5765ccb4ac7bb29104bd4f5
maxiaowen <332168460@qq.com>
2017-05-20 09:20:17 +0800

    Initial commit

---
+++ app/src/main/java/com/atguigu/a321video02/pager/NetAudioPager.java
@@ -0,0 +1,33 @@
+package com.atguigu.a321video02.pager;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.atguigu.a321video02.fragment.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/5/20.
+ */
+
+public class NetAudioPager extends BaseFragment {
+
+    private TextView textView;
+
+
+    @Override
+    public View initView() {
+
+        textView = new TextView(context);
+        textView.setTextSize(30);
+        textView.setTextColor(Color.RED);
+        textView.setGravity(Gravity.CENTER);
+        return textView;
+    }
+
+    public void initData(){
+        super.initData();
+        textView.setText("我是网络音乐");
+    }
+}
